ae545f515865e4f3367b4eb1e146b0295b8b72a3
Vladimir <vladimir.ivinski89@gmail.com>
2021-08-14 15:45:12 +0300

    WIP: create header for work bench

--- workbenchApp/force-app/main/default/lwc/workbenchApp/workbenchApp.js
+++ workbenchApp/force-app/main/default/lwc/workbenchApp/workbenchApp.js
@@ -1,3 +1,6 @@
 import { LightningElement } from "lwc";
+import mySVG_icon from "@salesforce/resourceUrl/workbenchStaticResource";
 
-export default class WorkbenchApp extends LightningElement {}
+export default class WorkbenchApp extends LightningElement {
+  warning_blueIcon = mySVG_icon + "#warning_blue";
+}
