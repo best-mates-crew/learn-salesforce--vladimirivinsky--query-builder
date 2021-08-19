eeab279ddc0937d8514705c33fde69779252d917
Vladimir <vladimir.ivinski89@gmail.com>
2021-08-19 11:10:47 +0300

    WIP: replaced checkbox and combobox

--- force-app/main/default/lwc/viQueryBuilder/viQueryBuilder.js
+++ force-app/main/default/lwc/viQueryBuilder/viQueryBuilder.js
@@ -1,32 +1,5 @@
 import { LightningElement} from "lwc";
-import { loadStyle } from "lightning/platformResourceLoader";
-import workbenchStaticResource from "@salesforce/resourceUrl/workbenchStaticResource";
-import getSObjects from "@salesforce/apex/ObjectInfoRetrieve.getSObjects";
 
 export default class ViQueryBuilder extends LightningElement {
-    viewOptions = [];
-
-    vOptions = [
-        { label: "List", value: "List" },
-        { label: "Matrix", value: "Matrix" },
-        { label: "Bulk", value: "Bulk" },
-        { label: "CSV", value: "CSV" },
-        { label: "Bulk XML", value: "Bulk XML" }
-    ];
-
-    archiveOptions = [
-        { label: "Exclude", value: "Exclude" },
-        { label: "Include", value: "Include" }
-    ];
-
-     async connectedCallback() {
-        await loadStyle(this, workbenchStaticResource + "/styles/main.css");
-        this.viewOptions = await getSObjects();
-        console.log("ok")
-    }
-
-    handleChange(event) {
-      this.value = event.detail.value;
-  }
 
 }
